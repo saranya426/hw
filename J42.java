891cffb7d7073a1b85ae22461bdd8ba167f9eeaa
saranya426 <saranchell96@gmail.com>
2025-07-28 09:51:08 +0530

    Add files via upload

---
+++ J42.java
@@ -0,0 +1,17 @@
+
+package j42;
+
+import demo.Democlass;
+
+
+public class J42 {
+
+    public static void main(String[] args) {
+//       Democlass ki = new Democlass();
+//       ki.display();
+        Today to = new Today();
+        to.greetings();
+       
+    }
+    
+}
